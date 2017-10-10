8075e6bf2b859d8c790cabe178d85bf87ef0eb7b
wcl14 <wangchunling14@126.com>
2017-10-10 12:19:35 +0800

    Move some structure from tpch1.c to tpch1.h

--- contrib/hawq-handcode/tpch1.c
+++ contrib/hawq-handcode/tpch1.c
@@ -7,56 +7,17 @@
 #include "cdb/cdbparquetrowgroup.h"
 #include "utils/memutils.h"
 #include "utils/palloc.h"
-#include "../backend/access/parquet/parquetam.c"
-#include "../backend/cdb/cdbparquetcolumn.c"
-
 #include "snappy-c.h"
 #include "zlib.h"
+#include "../backend/access/parquet/parquetam.c"
+#include "../backend/cdb/cdbparquetcolumn.c"
+#include "tpch1.h"
 
 extern text *cstring_to_text(const char *s);
 
 PG_MODULE_MAGIC;
 
 PG_FUNCTION_INFO_V1(runtpch1);
-
-
-#define BUFFER_SCALE_FACTOR	1.2
-#define BUFFER_SIZE_LIMIT_BEFORE_SCALED ((Size) ((MaxAllocSize) * 1.0 / (BUFFER_SCALE_FACTOR)))
-#define BUFFER_SIZE 1024
-#define TMP_COLUMNS 2
-#define relname "lineitem"
-#define MAX_TUPLE_NUM 10000000
-
-typedef struct lineitem_for_query1{
-    double  l_quantity;
-    double  l_extendedprice;
-    double  l_discount;
-    double  l_tax;
-    char    l_returnflag;
-    char    l_linestatus;
-    char    l_shipdate[11];
-} lineitem_for_query1;
-
-lineitem_for_query1 read_tuples[MAX_TUPLE_NUM];
-int	total_tuples_num = 0;
-bool projs[16] = {0};
-
-typedef struct SegFile{
-    char    filePath[1000];
-    File    file;
-    ParquetMetadata parquetMetadata;
-    CompactProtocol *footerProtocol;
-    int     rowGroupCount;
-    int     rowGroupProcessedCount;
-} SegFile;
-
-typedef struct ParquetFormatScan{
-    Relation    rel;
-    SegFile    *segFile;
-    TupleDesc   pqs_tupDesc;
-    int         *hawqAttrToParquetColChunks;
-    ParquetRowGroupReader   rowGroupReader;
-} ParquetFormatScan;
 
 static File DoOpenFile(char *filePath)
 {
