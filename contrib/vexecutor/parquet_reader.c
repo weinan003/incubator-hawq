1d27a2532246c0e550b8e2ef2ab7445581c93e3f
Ivan <wengyanqing@gmail.com>
2017-10-31 12:03:24 +0800

    parquet scan by tuple batch and add some debugging function

--- contrib/vexecutor/parquet_reader.c
+++ contrib/vexecutor/parquet_reader.c
@@ -1,7 +1,17 @@
 #include "parquet_reader.h"
+#include "tuple_batch.h"
+#include "debug.h"
+
+extern bool getNextRowGroup(ParquetScanDesc scan);
 
 static TupleTableSlot *ExecParquetScanRelation(ScanState *node);
 static TupleTableSlot *ParquetVScanNext(ScanState *scanState);
+static void parquet_vgetnext(ParquetScanDesc scan, ScanDirection direction, TupleTableSlot *slot); 
+static int ParquetRowGroupReader_ScanNextTupleBatch(TupleDesc tupDesc,
+							ParquetRowGroupReader *rowGroupReader,
+							int *hawqAttrToParquetColNum,
+							bool *projs,
+							TupleTableSlot *slot);
 
 TupleTableSlot *
 ExecParquetVScan(TableScanState *node)
@@ -132,6 +142,174 @@ ParquetVScanNext(ScanState *scanState)
 	ParquetScanState *node = (ParquetScanState *)scanState;
 	Assert(node->opaque != NULL && node->opaque->scandesc != NULL);
 
-	parquet_getnext(node->opaque->scandesc, node->ss.ps.state->es_direction, node->ss.ss_ScanTupleSlot);
+	parquet_vgetnext(node->opaque->scandesc, node->ss.ps.state->es_direction, node->ss.ss_ScanTupleSlot);
 	return node->ss.ss_ScanTupleSlot;
+}
+
+void 
+parquet_vgetnext(ParquetScanDesc scan, ScanDirection direction, TupleTableSlot *slot) 
+{
+
+	AOTupleId aoTupleId;
+	Assert(ScanDirectionIsForward(direction));
+
+	for(;;)
+	{
+		if(scan->bufferDone)
+		{
+			/*
+			 * Get the next row group. We call this function until we
+			 * successfully get a block to process, or finished reading
+			 * all the data (all 'segment' files) for this relation.
+			 */
+			while(!getNextRowGroup(scan))
+			{
+				/* have we read all this relation's data. done! */
+				if(scan->pqs_done_all_splits)
+				{
+					ExecClearTuple(slot);
+					return /*NULL*/;
+				}
+			}
+
+			StringInfo message = ParquetScanDescToString(scan);
+			elog(LOG, "\n%s", message->data);
+
+			scan->bufferDone = false;
+		}
+
+		//dumpRowGroup(scan);
+		int row_num  = ParquetRowGroupReader_ScanNextTupleBatch(
+								scan->pqs_tupDesc,
+								&scan->rowGroupReader,
+								scan->hawqAttrToParquetColChunks,
+								scan->proj,
+								slot);
+		if(row_num > 0)
+		{
+			elog(LOG, "ParquetRowGroupReader_ScanNextTupleBatch: row_num:%d", row_num);
+/*
+			int segno = ((FileSplitNode *)list_nth(scan->splits, scan->pqs_splits_processed - 1))->segno;
+			AOTupleIdInit_Init(&aoTupleId);
+			AOTupleIdInit_segmentFileNum(&aoTupleId, segno);
+
+			scan->cur_seg_row++;
+			AOTupleIdInit_rowNum(&aoTupleId, scan->cur_seg_row);
+
+			scan->cdb_fake_ctid = *((ItemPointer)&aoTupleId);
+
+			slot_set_ctid(slot, &(scan->cdb_fake_ctid));
+*/
+			// set tuple batch to TupleTableSlot
+
+
+			return;
+		}
+
+		/* no more items in the row group, get new buffer */
+		scan->bufferDone = true;
+	}
+}
+
+/*
+ * Get next tuple batch from current row group into slot.
+ *
+ * Return false if current row group has no tuple left, true otherwise.
+ */
+int
+ParquetRowGroupReader_ScanNextTupleBatch(
+	TupleDesc 				tupDesc,
+	ParquetRowGroupReader	*rowGroupReader,
+	int						*hawqAttrToParquetColNum,
+	bool 					*projs,
+	TupleTableSlot 			*slot)
+{
+	Assert(slot);
+
+	if (rowGroupReader->rowRead >= rowGroupReader->rowCount)
+	{
+		ParquetRowGroupReader_FinishedScanRowGroup(rowGroupReader);
+		return false;
+	}
+
+	/*
+	 * get the next item (tuple) from the row group
+	 */
+	rowGroupReader->rowRead = rowGroupReader->rowCount;
+	int nrow = rowGroupReader->rowCount;
+	int ncol = slot->tts_tupleDescriptor->natts;
+	
+	TupleBatch tb = createTupleBatch(nrow, ncol);
+	slot->PRIVATE_tts_data = (void *)tb;
+
+	int colReaderIndex = 0;
+	for(int i = 0; i < tb->ncol; i++)
+	{
+		TupleColumnData *tc = getTupleBatchColumn(tb, i);
+		Datum 	*values = tc->values;
+		bool	*nulls = tc->nulls; 
+		
+		if(projs[i] == false)
+		{
+			nulls[i] = true;
+			continue;
+		}
+
+		ParquetColumnReader *nextReader =
+			&rowGroupReader->columnReaders[colReaderIndex];
+		int hawqTypeID = tupDesc->attrs[i]->atttypid;
+		
+		for(int j=0;j<tb->nrow;j++)
+		{
+		 
+			if(hawqAttrToParquetColNum[i] == 1)
+			{
+				ParquetColumnReader_readValue(nextReader, &values[j], &nulls[j], hawqTypeID);
+				elog(WARNING, "value:%d null:%d", DatumGetInt32(values[j]), nulls[j]);
+			}
+			else
+			{
+				/*
+				 * Because there are some memory reused inside the whole column reader, so need
+				 * to switch the context from PerTupleContext to rowgroup->context
+				 */
+				MemoryContext oldContext = MemoryContextSwitchTo(rowGroupReader->memoryContext);
+
+				switch(hawqTypeID)
+				{
+					case HAWQ_TYPE_POINT:
+						ParquetColumnReader_readPoint(nextReader, &values[j], &nulls[j]);
+						break;
+					case HAWQ_TYPE_PATH:
+						ParquetColumnReader_readPATH(nextReader, &values[j], &nulls[j]);
+						break;
+					case HAWQ_TYPE_LSEG:
+						ParquetColumnReader_readLSEG(nextReader, &values[j], &nulls[j]);
+						break;
+					case HAWQ_TYPE_BOX:
+						ParquetColumnReader_readBOX(nextReader, &values[j], &nulls[j]);
+						break;
+					case HAWQ_TYPE_CIRCLE:
+						ParquetColumnReader_readCIRCLE(nextReader, &values[j], &nulls[j]);
+						break;
+					case HAWQ_TYPE_POLYGON:
+						ParquetColumnReader_readPOLYGON(nextReader, &values[j], &nulls[j]);
+						break;
+					default:
+						/* TODO array type */
+						/* TODO UDT */
+						Insist(false);
+						break;
+				}
+
+				MemoryContextSwitchTo(oldContext);
+			}
+		}
+
+		colReaderIndex += hawqAttrToParquetColNum[i];
+	}
+
+	/*construct tuple, and return back*/
+	TupSetVirtualTupleNValid(slot, ncol);
+	return tb->nrow;
 }
